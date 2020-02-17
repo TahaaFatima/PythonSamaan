465aa51a8ba39cf9f157e662c0f9aba5b89e571f
TahaaFatima <tahafatima95@gmail.com>
2020-02-17 16:53:56 +0500

    templates - django

--- sessionTwentyThree/mysite/polls/views.py
+++ sessionTwentyThree/mysite/polls/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world.")
+    latest_question_list = Question.objects.all()
+    context = {'latest_question_list': latest_question_list}
+    return render(request,'index.html',context)
+
+def detail(request, question_id):
+    """ return HttpResponse(f"You're looking at {question_id}") """
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'detail.html', {'question': question})
+
+def results(request, question_id):
+    response = f"you're looking at results of question {question_id}"
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
