d3353b2b18ecf8bafefacbb39087a89c34a4a25a
TahaaFatima <tahafatima95@gmail.com>
2020-02-26 15:13:07 +0500

    django complete test

--- test/website/uliya/views.py
+++ test/website/uliya/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Testimonials,Slider
+from .models import Testimonials,Slider,UserDetailsForm
 
 # Create your views here.
 def index(request):
@@ -9,6 +9,15 @@ def index(request):
     context = {'image_details': image_details,
                 'slider_details': slider_details
               }
+
+    if request.method == "POST":
+        form = UserDetailsForm()
+        form.user_name         = request.POST.get('name')
+        form.user_email        = request.POST.get('email')
+        form.user_phone_number = request.POST.get('phone_number')
+        form.user_message      = request.POST.get('message')
+        form.save()
+
     return render(request,"index.html",context)
 
 def about(request):
